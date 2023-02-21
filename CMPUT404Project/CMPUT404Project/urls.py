db62b3d35538246dc9d980a0e0fb56eddd6510cd
Alex-Mak-MCW <77552818+Alex-Mak-MCW@users.noreply.github.com>
2023-02-21 12:40:51 -0700

    before merge

--- CMPUT404Project/CMPUT404Project/urls.py
+++ CMPUT404Project/CMPUT404Project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     # path('', include('socialDist.urls')),
     path('admin/', admin.site.urls),
+    path("user/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('api/', include('socialDist.urls')),
 ]
