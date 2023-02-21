f36f076faf54c3552cdbadc332ba6c21efa13cfe
Alex-Mak-MCW <77552818+Alex-Mak-MCW@users.noreply.github.com>
2023-02-20 21:25:58 -0700

    added more models

--- CMPUT404Project/CMPUT404Project/settings.py
+++ CMPUT404Project/CMPUT404Project/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# avoid primary key issue:
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
--- CMPUT404Project/CMPUT404Project/urls.py
+++ CMPUT404Project/CMPUT404Project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('socialDist.urls')),
+    # path('', include('socialDist.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('socialDist.urls')),
 ]
