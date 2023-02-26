ffbdd337e4ed90c3da38ccafa033b960f3d27e16
filippociandy10 <filippo.ciandy10@gmail.com>
2023-02-26 00:56:10 -0700

    Added post frontend functionality

--- CMPUT404Project/CMPUT404Project/urls.py
+++ CMPUT404Project/CMPUT404Project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("django.contrib.auth.urls")),
     path('user/settings', TemplateView.as_view(template_name='settings.html'), name='settings'),
+    path('user/post', TemplateView.as_view(template_name='post.html'), name='post'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('api/', include('socialDist.urls')),
 ]
