95fb5c55a45bd3520e6e0ba2bfb576cdc771c2f6
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2023-03-05 21:10:10 -0700

    test login page

--- CMPUT404Project/socialDist/tests.py
+++ CMPUT404Project/socialDist/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class AccountsUITest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+    def test_login_page(self):
+        url = reverse('login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertContains(response, 'Log In')
