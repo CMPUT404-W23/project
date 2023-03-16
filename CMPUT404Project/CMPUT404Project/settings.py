10b889de283a08282b6d38f7d1e5487109b23ba7
jaskim9824 <jaskim9824@gmail.com>
2023-03-16 14:33:41 -0600

    deploying to heroku

--- CMPUT404Project/CMPUT404Project/settings.py
+++ CMPUT404Project/CMPUT404Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku # top of the file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,5 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login"
+
+django_on_heroku.settings(locals()) # bottom of the file
