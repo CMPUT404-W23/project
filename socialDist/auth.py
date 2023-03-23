548c965fb8fe4733a86e4efcad5dcc60207e55df
jaskim9824 <jaskim9824@gmail.com>
2023-03-22 22:06:49 -0600

    modified permissions to match spec

--- socialDist/auth.py
+++ socialDist/auth.py
@@ -26,6 +26,8 @@ from . import models
 # Sources:
 # https://www.django-rest-framework.org/api-guide/permissions/#custom-permissions
 
+# File that contains the permission classes used to restrict node access to specifc endpoints
+
 HOST="socialcmput404.herokuapp.com"
 
 # Permission class used for most endpoints to restrict remote node access
@@ -40,23 +42,46 @@ class RemotePermission(permissions.BasePermission):
             if authorizationArr[0] != "Token":
                 return False
             server = models.Server.objects.get(serverKey=authorizationArr[1])
+            # Remote server not able to access non GET methods
             if request.method not in permissions.SAFE_METHODS and not server.isLocalServer:
                 return False
             return True
         except:
             return False
 
-# Permission class used for the inbox endpoints to restrict remote node access
-class InboxPermission(permissions.BasePermission):
+# Permission class used for the comments endpoint to restrict remote node access
+class CommentsPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
+        # Special permission for admin users, can use API without auth token
+        if request.user.is_authenticated and request.user.is_staff:
+            return True
         try:
             authorization = request.headers['Authorization']
             authorizationArr = authorization.split()
             if authorizationArr[0] != "Token":
                 return False
             server = models.Server.objects.filter(serverKey=authorizationArr[1])
-            #TODO: modify to match inbox permissions
-            if request.method not in permissions.SAFE_METHODS and not server.isLocalServer:
+            # We must allow remote modes to POST to /comments to allow them to post comments!
+            if (request.method not in permissions.SAFE_METHODS and request.method != "POST") and not server.isLocalServer:
+                return False
+            return True
+        except:
+            return False
+
+# Permission class used for the inbox endpoint to restrict remote node access
+class InboxPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # Special permission for admin users, can use API without auth token
+        if request.user.is_authenticated and request.user.is_staff:
+            return True
+        try:
+            authorization = request.headers['Authorization']
+            authorizationArr = authorization.split()
+            if authorizationArr[0] != "Token":
+                return False
+            server = models.Server.objects.filter(serverKey=authorizationArr[1])
+            # Inbox, remote nodes can only POST!
+            if (request.method == "GET" or request.method == "DELETE") and not server.isLocalServer:
                 return False
             return True
         except:
