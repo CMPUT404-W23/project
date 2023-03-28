3e691d1ba389c0352b2f856123338975786be79f
jaskim9824 <jaskim9824@gmail.com>
2023-03-28 16:16:45 -0600

    adding new hosts to CORS whitelist

--- CMPUT404Project/settings.py
+++ CMPUT404Project/settings.py
@@ -161,7 +161,9 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:8000",
 "http://127.0.0.1:8000",
 "https://social-t30.herokuapp.com",
-"https://sd7-api.herokuapp.com"
+"https://sd7-api.herokuapp.com",
+"https://ultimate-teapot.herokuapp.com",
+"https://distributed-social-net.herokuapp.com"
 ]
 
 # Static files (CSS, JavaScript, Images)
