d4b6cb4b6aa69994d4f440dff2be396a11cf304a
jaskim9824 <jaskim9824@gmail.com>
2023-04-06 09:49:25 -0600

    more docs stuff

--- socialDist/models.py
+++ socialDist/models.py
@@ -23,7 +23,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Models used inour appilcation
+# Models used in our appilcation
 
 # Sources:
 # https://docs.djangoproject.com/en/dev/topics/auth/customizing/#extending-the-existing-user-model
